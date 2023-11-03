4ea530e88ea1227189678f3385219785802df7b3
Mujtaba <mujtabajafri3@gmail.com>
2023-11-03 05:03:43 +0500

    admin P2

--- Scrapper/scrapper.py
+++ Scrapper/scrapper.py
@@ -75,7 +75,7 @@ def get_data(urlPortal):
             except:
                 new_dict['estimatedBid'] = 'Not Found'
             new_dict['CategoriesList'] = []
-            for cats in bid:
+            for cats in info['included']:    
                 new_dict['CategoriesList'].append(cats['id'] + ' = ' + cats['categoryName'])
             new_dict['url'] = bid_url
             print(new_dict)
