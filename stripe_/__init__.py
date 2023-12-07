7198adece9c1467510a44159bdf831535364d51e
Mujtaba <mujtabajafri3@gmail.com>
2023-12-08 03:23:51 +0500

    cleaned data

--- stripe_/__init__.py
+++ stripe_/__init__.py
@@ -61,8 +61,8 @@ def handle_checkout_session(session):
 
 
 def fetch_subscription_data(client_reference_id):
-    # customer = stripe_customer.find_one({"client_reference_id": client_reference_id})
-    customer = stripe_customer.find({"client_reference_id": client_reference_id})
+    customer = stripe_customer.find_one({"client_reference_id": client_reference_id})
+    # customer = stripe_customer.find({"client_reference_id": client_reference_id})
 
     # if record exists, add the subscription info to the render_template method
     if customer:
