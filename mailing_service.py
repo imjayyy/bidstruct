d6d84ab7e628b605a5c73c667c5ec08e5e65c400
Mujtaba <mujtabajafri3@gmail.com>
2023-12-19 02:03:06 +0500

    mailing list

--- mailing_service.py
+++ mailing_service.py
@@ -3,7 +3,8 @@ from admin.mailing_list import Mailing_Clients
 from main import app
 
 with app.app_context():
-    # Mailing_Clients().start_mailing_service()
+    mailing_service = Mailing_Clients()
+    mailing_service.get_all_portal_data()
+    mailing_service.start_mailing_service()
 
 
-    Mailing_Clients().send_confirmation_email('webcroz03@gmail.com', 'https://www.google.com/')
